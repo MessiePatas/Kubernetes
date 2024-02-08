87f1db863dc6b195fcc6bf74f1caf6fb16bae953
Ivangutierrez10 <ivangues@unisabana.edu.co>
2024-02-08 00:50:09 -0500

    Solución a los problemas

--- demo/demo/src/main/java/com/unisabana/juaneivan/Entity/Estudiante.java
+++ demo/demo/src/main/java/com/unisabana/juaneivan/Entity/Estudiante.java
@@ -1,24 +1,15 @@
 package com.unisabana.juaneivan.Entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import lombok.AllArgsConstructor;
+import javax.persistence.*;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "estudiantes")
 public class Estudiante {
     @Id
     @GeneratedValue
     private int id;
+    @Column
     private String name;
-
-
 }
